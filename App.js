e595d8a1d5259c0c94ac1cfb2197bbb313e5222b
david <github@djs.gg>
2022-03-22 13:36:36 -0400

    add pressable

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Big Testesssss</Text>
+      <Pressable style={styles.button} onPress={() =>{
+        console.log("hellothere");
+        alert("hello")}}> 
+        <Text style={styles.text}>Hello</Text> 
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
